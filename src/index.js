2bddfe357f7ad21505913c513e34aa9798409f57
Ashleigh Christopher <ashleighc207@gmail.com>
2019-10-25 12:34:21 -0400

    add in mongo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
+
+dotenv.config();
+
+console.log(process.env.MONGO_PASS);
+
+const mongoURI = `mongodb+srv://admin:${process.env.MONGO_PASS}@track-server-ojhkt.mongodb.net/test?retryWrites=true&w=majority`;
 
 app.get("/", (req, res) => {
   res.send("Success");
