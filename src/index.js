ba7a7ac6fbf26cc4bb5c808ccffaef1b0443db4f
Ashleigh Christopher <ashleighc207@gmail.com>
2019-10-25 12:40:52 -0400

    remove uri and add as env var

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const dotenv = require("dotenv");
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 dotenv.config();
 
-console.log(process.env.MONGO_PASS);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 
-const mongoURI = `mongodb+srv://admin:${process.env.MONGO_PASS}@track-server-ojhkt.mongodb.net/test?retryWrites=true&w=majority`;
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB...");
+});
+
+mongoose.connection.on("error", err => {
+  console.error("Failed to connect to MongoDB: ", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Success");
